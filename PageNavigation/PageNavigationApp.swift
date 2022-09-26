8ebc8765e7eb9081fa5117299dcd758d354e3da8
czp356 <czp356@gmail.com>
2022-09-26 12:02:24 +0800

    Initial Commit

---
+++ PageNavigation/PageNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageNavigationApp.swift
+//  PageNavigation
+//
+//  Created by 常志平 on 26/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PageNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
